048bfff5bd09d1f4b5dc5cf35b8244b78bea85e5
Lucia Aguirre <guerrolucia@gmail.com>
2025-04-16 10:52:19 +0200

    adds basic layout and dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import { Button } from "@/components/ui/button"
+import { BrowserRouter} from "react-router-dom";
+import "./App.css";
+import Layout from "./components/layout";
+import { ThemeProvider } from "./context/theme-provider";
+
  
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
-      <Button>Click me</Button>
-    </div>
-  )
+ <BrowserRouter>
+<ThemeProvider defaultTheme="dark">
+ <Layout>
+    Hello 
+ </Layout>
+ </ThemeProvider>
+ </BrowserRouter>
+  );
 }
  
 export default App
---
+++ src/components/layout.tsx
@@ -0,0 +1,16 @@
+import type { PropsWithChildren } from "react";
+
+const Layout = ({children}: PropsWithChildren) => {
+    return <div className="bg-gradient-to-br from-background to-muted">
+        <header>Header</header>
+        <main className="min-h-screen container mx-auto px-4 py-8">
+        {children}
+        </main>
+        <footer className="border-t backdrop-blur py-12">
+            <div className="container mx-auto px-4 text-center text-gray-400">Lucía Aguirre Rota</div>
+        </footer>
+        </div>
+
+}
+
+export default Layout
