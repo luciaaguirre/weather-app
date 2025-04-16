44ce8c47b3e841457f22dafb3110332cfba23559
Lucia Aguirre <guerrolucia@gmail.com>
2025-04-16 18:06:55 +0200

    feat: adds dark mode/ light mode functionality

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,16 @@
+import { useTheme } from "@/context/theme-provider";
+import { Moon, Sun } from "lucide-react";
 import { Link } from "react-router-dom";
 
 const Header =() => {
+   const {theme, setTheme} = useTheme();
+   const isDark = theme === "dark"
+
     return <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur py-2 supports-[backdrop-filter]:bg-background/60">
         <div className="container mx-auto flex h-16 items-center justify-between px-4">
             <Link to={"/"}>
             <img 
-            src="/rain.png"
+            src={ theme === "dark" ? "/logo.png" : "/logo2.png"}
             alt="cloud logo"
             className="h-14"
             />
@@ -13,6 +18,13 @@ const Header =() => {
             <div>
                 {/*search*/}
                  {/*theme toggle*/}
+                 <div onClick={() => setTheme(isDark ? "light" : "dark")}> 
+                    {isDark? (<Sun className=" h-6 w-6 text-yellow-500 rotate-0 transition-all"/> 
+                     ) : (
+                        <Moon className="h-6 w-6 text-blue-500 rotate-0 transition-all"/>
+
+                    )}
+                 </div>
             </div>
         </div>
     </header>
