ad5153c5180233d10065041791dc095b2b7510c5
Lucia Aguirre <guerrolucia@gmail.com>
2025-04-19 16:54:40 +0200

    imports tanstack query

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,12 @@ import Layout from "./components/layout";
 import { ThemeProvider } from "./context/theme-provider";
 import WeatherDashboard from "./pages/weather-dashboard"
 import CityPage from "./pages/city-page";
+import { QueryClientProvider} from "@tanstack/react-query"
  
 function App() {
   return (
+    <QueryClientProvider client={queryClient}>
+
  <BrowserRouter>
 <ThemeProvider defaultTheme="dark">
  <Layout>
@@ -18,6 +21,7 @@ function App() {
  </Layout>
  </ThemeProvider>
  </BrowserRouter>
+ </QueryClientProvider>
   );
 }
  
