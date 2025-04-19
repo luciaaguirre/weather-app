6fe5ac31b1b1541a9caadc2d2d8010c1e080e241
Lucia Aguirre <guerrolucia@gmail.com>
2025-04-19 23:02:00 +0200

    adds skeleton and alerts to dashboard

--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -1,6 +1,8 @@
 import { Button } from "@/components/ui/button";
 import { useGeolocation } from "@/hooks/use-geolocation";
-import {  RefreshCw } from "lucide-react";
+import {  AlertTriangle, MapPin, RefreshCw } from "lucide-react";
+import WeatherSkeleton from "@/components/loading-skeleton";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 
 const WeatherDashboard = () => {
     //custom hook
@@ -19,7 +21,39 @@ const WeatherDashboard = () => {
     };
 
     if (locationLoading) {
-        
+        return <WeatherSkeleton/>
+    }
+
+    if (locationError) {
+        return (
+     <Alert variant="destructive">
+      <AlertTriangle className="h-4 w-4" />
+      <AlertTitle> Location Error </AlertTitle>
+      <AlertDescription className="flex flex-col gap-4">
+        <p>{locationError}</p>
+        <Button onClick={getLocation} variant= {"outline"} className="w-fit">
+            <MapPin className="mr-2 h-4 w-4"/>
+            Enable Location
+        </Button>
+      </AlertDescription>
+    </Alert>
+        )
+    }
+
+    // if coordinates are not provided
+    if (!coordinates) {
+        return (
+     <Alert variant="destructive">
+      <AlertTitle> Location Required </AlertTitle>
+      <AlertDescription className="flex flex-col gap-4">
+        <p>Please enable location access to see your local weather</p>
+        <Button onClick={getLocation} variant= {"outline"} className="w-fit">
+            <MapPin className="mr-2 h-4 w-4"/>
+            Enable Location
+        </Button>
+      </AlertDescription>
+    </Alert>
+        )
     }
 
 
