92dc18e8d8d24ebf83363a28f0352c28161033b0
Lucia Aguirre <guerrolucia@gmail.com>
2025-04-19 21:06:07 +0200

    adds custom hook to get geolocation

--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -1,4 +1,24 @@
+import { Button } from "@/components/ui/button";
+import {  RefreshCw } from "lucide-react";
+
 const WeatherDashboard = () => {
-    return <div>Weather Dashboard</div>
+    //custom hook
+
+
+    return <div className="space-y-4">
+        {/* Favorite Cities*/}
+        <div className="flex items-center justify-between">
+            <h1 className="text-xl font-bold tracking-tight">My Location</h1>
+            <Button variant={'outline'}
+            size={"icon"}
+            //onClick={handleRefresh}
+            //disabled={}
+            >
+               <RefreshCw className="h-4 w-4"/>
+            </Button>
+        </div>
+
+        {/* Current and Hourly Weather */}
+    </div>
 }
 export default WeatherDashboard; 
