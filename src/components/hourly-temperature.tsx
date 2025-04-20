abaa01a473370a18975785b5b53935dbd7b8607e
Lucia Aguirre <guerrolucia@gmail.com>
2025-04-20 20:12:01 +0200

    adds tooltip to chart

--- src/components/hourly-temperature.tsx
+++ src/components/hourly-temperature.tsx
@@ -1,7 +1,8 @@
 import { ForecastData } from "@/api/types";
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
-import {Line, LineChart, ResponsiveContainer, XAxis, YAxis} from "recharts";
+import {Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis} from "recharts";
 import {format} from "date-fns"
+import { Divide } from "lucide-react";
 
 
 interface HourlyTemperatureProps {
@@ -41,13 +42,41 @@ const HourlyTemperature = ({data}: HourlyTemperatureProps) => {
                     tickFormatter={ (value) => `${value}º`}
                 />
                 {/* tooltip */}
+                <Tooltip
+                content={({ active, payload}) => {
+                    if(active && payload && payload.length){
+                        return(
+                            <div className="rounded-lg border bg-background p-2 shadow-sm">
+                                <div className="grid grid-cols-2 gap-2">
+                                    <div className="flex flex-col">
+                                        <span className="text-[0.70rem] uppercase text-muted-foreground">Temperature</span>
+                                        <span className="font-bold">{payload[0].value}º</span>
+                                    </div>
+                                    <div className="flex flex-col">
+                                        <span className="text-[0.70rem] uppercase text-muted-foreground">Feels Like</span>
+                                        <span className="font-bold">{payload[1].value}º</span>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    }
+                }}
+                />
 
-                <Line
+                <Line //for temperature
                 type="monotone"
                 dataKey="temp"
                 stroke="#2563eb"
                 strokeWidth={2}
                 dot={false}
+                />
+                <Line // for feelslike temperature
+                type="monotone"
+                dataKey="feels_like"
+                stroke="#64748b"
+                strokeWidth={2}
+                dot={false}
+                strokeDasharray="5 5"
                 />
 
 
