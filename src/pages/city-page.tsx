1ceb0ea602350fc2958e22720d588d0695a80998
Lucia Aguirre <guerrolucia@gmail.com>
2025-04-23 14:27:40 +0200

    adds UI basics to citypage

--- src/pages/city-page.tsx
+++ src/pages/city-page.tsx
@@ -1,5 +1,9 @@
+import CurrentWeather from "@/components/current-weather";
+import HourlyTemperature from "@/components/hourly-temperature";
 import WeatherSkeleton from "@/components/loading-skeleton";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import WeatherDetails from "@/components/weather-details";
+import WeatherForecast from "@/components/weather-forecast";
 import { useForecastQuery, useWeatherQuery } from "@/hooks/use-weather";
 import { AlertTriangle } from "lucide-react";
 import { useParams, useSearchParams } from "react-router-dom";
@@ -32,8 +36,29 @@ const CityPage = () => {
     return <WeatherSkeleton />;
   }
 
-  return <div>
+  return <div className="space-y-4">
+  {/* Favorite Cities*/}
+  <div className="flex items-center justify-between">
+    <h1 className="text-3xl font-bold tracking-tight">{params.cityName}, {weatherQuery.data.sys.country}
+    </h1>
     
-  </div>;
+  </div>
+
+  <div className="grid gap-6">
+    <div className="flex flex-col  gap-4">
+      <CurrentWeather
+        data={weatherQuery.data}
+        
+      />
+
+      <HourlyTemperature data={forecastQuery.data} />
+    </div>
+
+    <div className="grid gap-6 md:grid-cols-2 items-start">
+      <WeatherDetails data={weatherQuery.data} />
+      <WeatherForecast data= {forecastQuery.data}/>
+    </div>
+  </div>
+</div>;
 };
 export default CityPage;
