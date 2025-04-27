ce1ce1460d14c12c08f6b4d7fa3ebe811260b9d9
Lucia Aguirre <guerrolucia@gmail.com>
2025-04-27 18:14:38 +0200

    adds favorite button UI, pending addFavorite functionality

--- src/components/favorite-button.tsx
+++ src/components/favorite-button.tsx
@@ -1,11 +1,23 @@
 import { WeatherData } from "@/api/types";
+import { useFavorite } from "@/hooks/use-favorite";
+import { Button } from "./ui/button";
+import { Star } from "lucide-react";
 
 interface FavoriteButtonProps {
     data: WeatherData;
 }
 
 const FavoriteButton = ({ data } : FavoriteButtonProps) => {
-    return <div>Favorite Button</div>
+    const { addFavorite, isFavorite, removeFavorite } = useFavorite();
+    const isCurrentlyFavorite = isFavorite(data.coord.lat, data.coord.lon)
+
+    return <Button variant={isCurrentlyFavorite ? "default" : "outline"} size={"icon"}
+    className={ isCurrentlyFavorite ? "bg-yellow-500 hover:bg-yellow-600" : ""}
+    >
+        <Star
+        className= {`h-4 w-4 ${isCurrentlyFavorite ? "fill-current" : ""}`}
+        />
+    </Button>
 };
 
 export default FavoriteButton
