f890cdfdfe739d0c1ea85d62d734486593700a37
Lucia Aguirre <guerrolucia@gmail.com>
2025-04-27 18:04:49 +0200

    creates fav button, missing UI

---
+++ src/components/favorite-button.tsx
@@ -0,0 +1,11 @@
+import { WeatherData } from "@/api/types";
+
+interface FavoriteButtonProps {
+    data: WeatherData;
+}
+
+const FavoriteButton = ({ data } : FavoriteButtonProps) => {
+    return <div>Favorite Button</div>
+};
+
+export default FavoriteButton
--- src/pages/city-page.tsx
+++ src/pages/city-page.tsx
@@ -1,4 +1,5 @@
 import CurrentWeather from "@/components/current-weather";
+import FavoriteButton from "@/components/favorite-button";
 import HourlyTemperature from "@/components/hourly-temperature";
 import WeatherSkeleton from "@/components/loading-skeleton";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
@@ -42,7 +43,7 @@ const CityPage = () => {
     <h1 className="text-3xl font-bold tracking-tight">{params.cityName}, {weatherQuery.data.sys.country}
     </h1>
     <div >
-      {/* favorite button */ }
+     <FavoriteButton data={ {...weatherQuery.data, name: params.cityName}}/>
     </div>
     
   </div>
